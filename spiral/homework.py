8cab9c5d1c53c0e52e8f42bc5b197ba44ef776d6
vcu-villalobosss <villalobosss@vcu.edu>
2021-04-09 23:49:46 -0400

    homework

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,9 @@
+# find sum of numbers located on diagonals
 def spiralize(number):
-    return_value = 1
+    # finding diagonals
+    x = (number - 1) / 2
+    # formula for sum of diagonals
+    y = (3 + 2 * x * (8 * x * x + 15 * x + 13)) / 3
 
+    return_value = y
     return return_value
